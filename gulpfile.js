a0bd9cb2fd33e7018c06bbdc7deac7527f1663fc
OleksiukIra <54508449+OleksiukIra@users.noreply.github.com>
2020-03-01 21:00:45 +0300

    site "smallars"

---
+++ gulpfile.js
@@ -0,0 +1,19 @@
+const gulp = require('gulp');
+const webserver = require('gulp-webserver');
+
+gulp.task('webserver', function() {
+  gulp.src('html')
+    .pipe(webserver({
+      livereload: false,
+      port:8000,
+      open: true
+    }));
+});
+
+
+gulp.task(
+  'default',
+  gulp.series([
+    'webserver'
+  ])
+);
